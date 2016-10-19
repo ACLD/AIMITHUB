4f8ce904b322129f5e0febda6c323e1bae77eed1
devd1029 <sawantdevdatt@gmail.com>
2016-10-19 18:17:13 +0530

    Revert "leave note"
    This reverts commit 90954e6da3390b26703b7bd9c14356ce6aa34839.

--- advance-admin/index.php
+++ advance-admin/index.php
@@ -149,7 +149,7 @@
                     </div>
 					<div class="col-md-4 shad">
                         <div class="main-box mb-blue effect2">
-                            <a href="leave.php">
+                            <a href="leave.html">
                                 <i ></i>
                                 <h5>Leave Applications</h5>
                             </a>
