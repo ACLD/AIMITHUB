c91da40098109f9cda626e895b1e203fcdf23a64
devd1029 <sawantdevdatt@gmail.com>
2016-11-09 09:59:36 -0800

    Revert "Signup Design"
    This reverts commit 0b021229f5c998123260490cd6b8e3b75a463803.

--- advance-admin/signup.php
+++ advance-admin/signup.php
@@ -15,21 +15,54 @@
 		
 </head>
 <body>
-	<div id="box1" class="box1 blurred-bg tinted">
-		<div class="content">
-			<form name="form2" method="post" action="signupuser.php"  id="sigup" enctype="multipart/form-data">
-				<a href="index.php" ><input type="Button" class="close1" value="X" ></a>
-				<input name="regnum" type="text" id="regnum"  placeholder="Reg Num" pattern="[0-9]+" required >
-				<input name="name" type="text" id="name"  placeholder="Full Name" pattern="[a-zA-Z ]+" required title="Insert only the alphabet">
-				<input type="password" name="pass" onblur="checkpass()" placeholder="Password" required>
-				<input name="password" type="password" id="password" placeholder="Confirm password" required> 
-				<input name="email" type="email" id="email" placeholder="Email" required title="Insert a valid email">
-				<input name="contact" type="text" id="contact" placeholder="Mobile No"pattern="[0-9]+" required  title="Insert only numbers ">
-				<div class="submit">
-							<input type="submit" id="btnSignUp" value="Sign-Up" >
-				</div>
-			</form>
-		</div>
-	</div>
+	<div id="box" class="box blurred-bg tinted">
+  <div class="content">
+   <h1 align="center"><span id="signuphead">New User Signup</span></h1>
+   <form name="form1" method="post" action="signupuser.php"  enctype="multipart/form-data">
+
+       <table class="sign">
+          <tr>
+				
+			  <td>
+					<input name="regnum" type="text" id="regnum"  placeholder="Reg Num" pattern="[0-9]+" required >
+			 </td>
+         </tr>
+	
+		  <tr>
+          
+           <td><input name="name" type="text" id="name"  placeholder="Full Name" pattern="[a-zA-Z ]+" required title="Insert only the alphabet"></td>
+         </tr>
+	     <tr>
+		     <td><input type="password" name="pass" onblur="checkpass()" placeholder="Password" required></td>
+		</tr>
+		   
+        <tr>
+		    <td><input name="password" type="password" id="password" placeholder="Confirm password" required></td>
+         </tr>
+		<tr>  
+           <td><input name="email" type="email" id="email" placeholder="Email" required title="Insert a valid email"></td>
+         </tr>
+	
+      
+         <tr>
+          
+            <td><input name="contact" type="text" id="contact" placeholder="Mobile No"pattern="[0-9]+" required  title="Insert only numbers "></td>
+         </tr>
+		 
+         <tr>
+           <td>
+		    <button type="submit" class="button" style="vertical-align:middle"><span>Sign-Up</span></button>
+           </td>
+         </tr>
+       </table>
+     </form>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>
+$(function() {
+	$( ".box" ).draggable();
+});
+</script>
 </body>
 
